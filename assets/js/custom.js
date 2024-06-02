45f11438b7d48d404c1171bac56c00c55d85305c
kudriakova21 <vkudriakova@gmail.com>
2024-06-02 13:06:20 +0200

    custom.js updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,30 +4,4 @@ $(document).ready(function () {
             $(this).toggleClass('active');
         });
     });
-
-    // $('#id').attr('color',)
-    // function showName() {
-    //     let name = 'viktoriia';
-    //     alert(name);
-    // }
-    // showName();
-
-    // function show(a) {
-    //     let x, y;
-    //     x = a;
-    //     y = x + 7;
-    //     alert(y);
-    // }
-    // show(5);
-
-    // let x = prompt('skolko let?');
-    // if (x >= 18) {
-    //     let imia = prompt('imia?');
-    //     alert('privet, ' + imia);
-    // } else {
-    //     alert('poka');
-    // }
-
-    // $('#id').addClass('red');
-
 });
