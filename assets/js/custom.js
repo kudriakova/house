5b466f10fb69559fa0054cc76314d1681a7e7e80
kudriakova21 <vkudriakova@gmail.com>
2024-07-21 14:10:54 +0200

    Added: countries galleries, filter buttons functionality

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
-    $('.filters__item').each(function () {
+    const buttons = $('.filters__item');
+    const blocks = $('.countries');
+    buttons.each(function () {
         $(this).on('click', function () {
             $(this).toggleClass('active');
+            if ($('.filters__item.active').length == 0) {
+                $(buttons[0]).addClass('active');
+            }
+            buttons.each(function (index) {
+                if ($(this).hasClass('active')) {
+                    $(blocks[index]).addClass('show');
+                } else {
+                    $(blocks[index]).removeClass('show');
+                }
+            });
         });
     });
 });
